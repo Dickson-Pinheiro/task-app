fdd2e99a6ff59042313a2e86d50c8544483e3081
Dickson-Pinheiro <dicksonpinheiro4@gmail.com>
2023-10-31 15:35:20 -0300

    feat: default and reset styles

---
+++ src/styles/theme/defaultTheme.ts
@@ -0,0 +1,11 @@
+export const defaultTheme = {
+    urgente: '#000000',
+    alta: '#ff0000',
+    media: '#ffa200',
+    baixa: '#0000ff',
+    'background-header-painel': 'linear-gradient(45deg, #e02523 10%, #661ca6 100%)',
+    'background-create-task-button': 'linear-gradient(45deg, #e02523 10%, #661ca6 100%)',
+    white: '#ffffff',
+    done: '#FACFCE',
+    'background-menu': '#443736'
+}
